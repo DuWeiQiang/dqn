cb844216da3cd5906d8431209478b43449ba0af7
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2014-12-18 16:42:18 -0600

    Added experimental directory to args of train script.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 set -e
 
-if [ $# -lt 1 ]; then
-    echo "usage: train.sh rom_file"
+if [ $# -lt 2 ]; then
+    echo "usage: train.sh rom_file exp_dir"
     exit
 fi
 
 EMAIL="mhauskn@cs.utexas.edu"
 ROM=$1
+EXP_DIR=$2
 ROM_NAME=`basename $ROM | awk -F'.bin' '{print $1}'`
-MAX_ITER=2000000
-TACC_ITER_PER_JOB=1000000
+
+# Create exp_dir
+mkdir -p $EXP_DIR
 
 if [[ `hostname` == *tacc* ]];
 then
+    MAX_ITER=10000000
+    TACC_ITER_PER_JOB=1000000
     iter=0
     i=0
     while [ $iter -lt $MAX_ITER ]
     do
         iter=$(($iter + $TACC_ITER_PER_JOB))
         if [ -z "$PID" ]; then
-            PID=$(cluster --suppress --gpu --outfile ${ROM_NAME}$i.out \
-                dqn -gpu -rom $ROM -snapshot_prefix state/$ROM_NAME$i \
+            PID=$(cluster --suppress --gpu --outfile $EXP_DIR/${ROM_NAME}$i.out \
+                dqn -gpu -rom $ROM -snapshot_prefix $EXP_DIR/$ROM_NAME$i \
                 -max_iter $TACC_ITER_PER_JOB)
         else
             PID=$(cluster --depend $PID --suppress --gpu \
-                --outfile ${ROM_NAME}$i.out dqn -gpu -rom $ROM \
+                --outfile $EXP_DIR/${ROM_NAME}$i.out dqn -gpu -rom $ROM \
                 -model state/$ROM_NAME$(($i-1))_iter_${TACC_ITER_PER_JOB}.caffemodel \
-                -snapshot_prefix state/$ROM_NAME$i \
+                -snapshot_prefix $EXP_DIR/$ROM_NAME$i \
                 -max_iter $TACC_ITER_PER_JOB \
                 -memory_threshold 50000 \
                 -explore 1)
@@ -37,5 +41,5 @@ then
     done
     cluster --suppress --depend $PID --email $EMAIL --gpu echo "$ROM_NAME Done!"
 else
-    cluster --suppress --gpu --email $EMAIL --outfile $ROM_NAME.out dqn -gpu -rom $ROM -snapshot_prefix state/$ROM_NAME -max_iter $MAX_ITER
+    cluster --suppress --gpu --email $EMAIL --outfile $EXP_DIR/$ROM_NAME.out dqn -gpu -rom $ROM -snapshot_prefix $EXP_DIR/$ROM_NAME -max_iter $MAX_ITER
 fi
