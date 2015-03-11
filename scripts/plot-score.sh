51f382aeb3faaa9de936e970c40907525c9671eb
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2015-03-11 16:21:43 -0500

    Added tight layout.

--- scripts/plot-score.sh
+++ scripts/plot-score.sh
@@ -5,5 +5,5 @@ if [ $# -lt 1 ]; then
     echo "usage: $0 log_file"
     exit
 fi
-grep score $1 | lmj-plot -m 'score = (\S+),.*' --xlabel Episode --ylabel Score --title $1 &
-grep Iteration $1 | lmj-plot -m 'Iteration (\d+), loss = (\S+)' --num-x-ticks 8 --xlabel Iteration --ylabel Loss --title $1 &
+grep score $1 | lmj-plot -m 'score = (\S+),.*' --xlabel Episode --ylabel Score --title $1 -g -T &
+grep Iteration $1 | lmj-plot -m 'Iteration (\d+), loss = (\S+)' --num-x-ticks 8 --xlabel Iteration --ylabel Loss --title $1 -g -T &
