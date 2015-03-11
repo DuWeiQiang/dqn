9259f210b5394806c6a9a5e58fac7d012ec3e0cd
Matthew Hausknecht <matthew.hausknecht@gmail.com>
2015-03-11 16:12:51 -0500

    Added plotting script to show score.

---
+++ scripts/plot-score.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 log_file"
+    exit
+fi
+grep score $1 | lmj-plot -m 'score = (\S+),.*' --xlabel Episode --ylabel Score --title $1 &
+grep Iteration $1 | lmj-plot -m 'Iteration (\d+), loss = (\S+)' --num-x-ticks 8 --xlabel Iteration --ylabel Loss --title $1 &
